1a39eee55fe04e4effab85951bbdd72076b7c73b
drdSchwarzenMagie <159145295+drdSchwarzenMagie@users.noreply.github.com>
2024-06-04 15:38:23 +0300

    Update test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -56,7 +56,7 @@ def test_pause_track(player, tmpdir):
 def test_stop_track(player):
     with patch('pygame.mixer.music.stop'):
         player.stop_track()
-    # Не нужно ничего проверять, просто убедимся, что нет исключений.
+    # Нет необходимости в проверках, просто убедимся, что исключения не возникают.
 
 def test_next_track(player, tmpdir):
     song1 = tmpdir.join("song1.mp3")
@@ -88,7 +88,7 @@ def test_toggle_mute(player):
         player.toggle_mute()
         mock_set_volume.assert_called_with(0.0)
 
-        # Сбросим мок
+        # Сбросим mock
         mock_set_volume.reset_mock()
 
         with patch('pygame.mixer.music.get_volume', return_value=0.0):
@@ -98,9 +98,8 @@ def test_toggle_mute(player):
 # Тестирование класса PlayerApp
 @pytest.fixture
 def player_app():
-    with patch('tkinter.Tk') as mock_tk:
-        mock_tk_instance = MagicMock()
-        mock_tk.return_value = mock_tk_instance
+    with patch('tkinter.Tk'), \
+         patch.object(PlayerApp, '__init__', lambda x, y: None):  # Мокируем метод __init__
         return PlayerApp()
 
 def test_load_directory(player_app, tmpdir):
@@ -108,4 +107,31 @@ def test_load_directory(player_app, tmpdir):
     shutil.copy(TEST_MP3_PATH, tmpdir.join("song2.mp3"))
     player_app.player.load_tracks(tmpdir)
     player_app.update_playlist()
-    assert player_app.playlist_box.size() ==
+    assert player_app.playlist_box.size() == 2
+
+def test_play_track_app(player_app, tmpdir):
+    song1 = tmpdir.join("song1.mp3")
+    shutil.copy(TEST_MP3_PATH, song1)
+    player_app.player.track_list = [Track(song1)]
+    player_app.play_track()
+    assert player_app.track_label.cget("text") == "Сейчас играет: song1.mp3"
+
+def test_next_track_app(player_app, tmpdir):
+    song1 = tmpdir.join("song1.mp3")
+    song2 = tmpdir.join("song2.mp3")
+    shutil.copy(TEST_MP3_PATH, song1)
+    shutil.copy(TEST_MP3_PATH, song2)
+    player_app.player.track_list = [Track(song1), Track(song2)]
+    player_app.player.current_track_index = 0
+    player_app.next_track()
+    assert player_app.track_label.cget("text") == "Сейчас играет: song2.mp3"
+
+def test_prev_track_app(player_app, tmpdir):
+    song1 = tmpdir.join("song1.mp3")
+    song2 = tmpdir.join("song2.mp3")
+    shutil.copy(TEST_MP3_PATH, song1)
+    shutil.copy(TEST_MP3_PATH, song2)
+    player_app.player.track_list = [Track(song1), Track(song2)]
+    player_app.player.current_track_index = 1
+    player_app.prev_track()
+    assert player_app.track_label.cget("text") == "Сейчас играет: song1.mp3"
